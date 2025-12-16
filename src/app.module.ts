15723978980629503128f2d88f1f1c731928c6d9
Alanissette16 <165698924+Alanissette16@users.noreply.github.com>
2025-12-15 19:25:59 -0500

    02_estrutura

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StatusModule } from './status/status.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [StatusModule, AuthModule, UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
